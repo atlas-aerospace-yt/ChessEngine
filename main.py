6ad621fc1ea7b02cc6f3bb0bb575ce97f06c8d23
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-19 14:54:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,24 @@
 main file to test the libraries while under development
 """
 
-from network.layer import Layer
+from network.network import NeuralNetwork
 from vector import activation
 from vector import Vector
 
-layer = Layer(10, 1, activation.sigmoid)
+if __name__ == "__main__":
 
-print(layer)
+    example_one = Vector([0,0,0,0,0,0,0,0,0,0])
+    example_two = Vector([1,0,0,0,1,0,0,0,0,0])
+    example_three = Vector([0,0,0,0,1,0,0,0,0,0])
 
-a = Vector([1, 2, 3, 4])
-b = Vector([5, 6, 7, 8])
+    network = NeuralNetwork(10, 1, 10, 10, activation.sigmoid)
 
-if float(a * b) != 70.0:
-    print("Error: multiplication failed!")
+    print(network.forward_propagation(example_one))
+    print(network.forward_propagation(example_two))
+    print(network.forward_propagation(example_three))
+
+    a = Vector([1, 2, 3, 4])
+    b = Vector([5, 6, 7, 8])
+
+    if float(a * b) != 70.0:
+        print("Error: multiplication failed!")
