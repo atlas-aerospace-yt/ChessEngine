12ae499f6b29debf35649fcceea8e775987aa6a3
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-19 22:54:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,13 @@ from vector import Vector
 
 if __name__ == "__main__":
 
-    example_one = Vector([0,0,0,0,0,0,0,0,0,0])
-    example_two = Vector([1,0,0,0,1,0,0,0,0,0])
-    example_three = Vector([0,0,0,0,1,0,0,0,0,0])
+    example_one = Vector([0,0])
+    example_two = Vector([1,0])
+    example_three = Vector([0,0])
 
-    network = NeuralNetwork(10, 1, 10, 10, activation.sigmoid)
+    network = NeuralNetwork(2, 1, 2, 2, activation.sigmoid, activation.sigmoid_prime)
 
-    network.forward_propagation(example_one)
-    print(network.outputs[-1])
-    network.forward_propagation(example_two)
-    print(network.outputs[-1])
-    network.forward_propagation(example_three)
-    print(network.outputs[-1])
+    network.cost_function_derivative(example_one, Vector([0]))
 
     a = Vector([1, 2, 3, 4])
     b = Vector([5, 6, 7, 8])
