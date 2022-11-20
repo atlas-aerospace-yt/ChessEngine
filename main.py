9fab22220ee9ae51ddcfff2c3b1dd1d63363feb9
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-20 19:56:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
 main file to test the libraries while under development
+
+TODO -> Overflow handling - more testing
 """
 
 from model.network import NeuralNetwork
@@ -12,16 +14,17 @@ if __name__ == "__main__":
     example_two = Vector([1,0])
     example_three = Vector([0,1])
 
-    network = NeuralNetwork((2, 1, 10, 10), activation.sigmoid, activation.sigmoid_prime)
+    network = NeuralNetwork((2, 1, 2, 2), activation.sigmoid, activation.sigmoid_prime)
 
-    print(network.forward_propagation(Vector([1,1])))
+    print(network.forward_propagation(example_two))
 
     for i in range(0, 1000):
         guess = network.backward_propagation(example_one, Vector([0]))
         guess = network.backward_propagation(example_two, Vector([1]))
         guess = network.backward_propagation(example_three, Vector([0]))
 
-    print(network.forward_propagation(Vector([1,0])))
+    print(network.forward_propagation(example_two))
+    print(network.forward_propagation(example_three))
 
     a = Vector([1, 2, 3, 4])
     b = Vector([5, 6, 7, 8])
