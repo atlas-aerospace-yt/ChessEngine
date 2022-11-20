21ef05aa187d776150125b7749660dd75bab6de0
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-20 23:02:06 +0000

    Update tests.py

--- unit_tests/tests.py
+++ unit_tests/tests.py
@@ -1,5 +1,26 @@
 """
 This file is designed to test features of the model and vector libraries within this repository
 """
+import os
+import sys
 
-assert 5 == 5
+parent = os.path.abspath('.')
+sys.path.insert(1, parent)
+
+from vector import Vector
+from vector import activation
+
+A = Vector([1,2,3,4])
+B = Vector([5,6,7,8])
+
+def test_multiplication():
+    """
+    Tests vector multiplication
+    """
+    assert float(A * B) == 70.0
+
+def test_sigmoid():
+    """
+    Tests the sigmoid function
+    """
+    assert activation.sigmoid(0) == 0.5
