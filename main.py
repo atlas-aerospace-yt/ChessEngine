b79acfc3418f38510a4d44ab034bfb72afcd3a0a
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-21 21:47:51 +0000

    Progress

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ main file to test the libraries while under development
 TODO -> Overflow handling - more testing
 """
 
+import matplotlib.pyplot as plt
+
 from model.network import NeuralNetwork
-from vector import activation
-from vector import Vector
+from vector import Vector, activation
 
 if __name__ == "__main__":
+
+    cost = []
 
     example_one = Vector([0,0,0,0])
     example_two = Vector([1,0,1,0])
     example_three = Vector([0,1,1,0])
 
-    network = NeuralNetwork((4, 1, 3, 3), activation.sigmoid, activation.sigmoid_prime)
+    network = NeuralNetwork((4, 1, 4, 10), activation.sigmoid, activation.sigmoid_prime)
 
-    for i in range(0, 10000):
+    for i in range(750):
         network.backward_propagation(example_one, Vector([0]))
         network.backward_propagation(example_two, Vector([1]))
         network.backward_propagation(example_three, Vector([0]))
@@ -28,6 +31,10 @@ if __name__ == "__main__":
 
         actual_output = [Vector(0), Vector(1), Vector(0)]
 
-        print(network.cost_function(actual_output, predicted_output))
+        cost.append(network.cost_function(actual_output, predicted_output))
 
+    print(network.forward_propagation(Vector([0,1,0,0])))
     print(network.forward_propagation(Vector([1,0,0,0])))
+
+    plt.plot(cost)
+    plt.show()
