142b5f8ab17364985c6ad91545bfb34cc83e659d
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-22 13:25:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,30 +11,36 @@ from vector import Vector, activation
 
 if __name__ == "__main__":
 
-    cost = []
-
     example_one = Vector([0,0,0,0])
     example_two = Vector([1,0,1,0])
     example_three = Vector([0,1,1,0])
 
-    network = NeuralNetwork((4, 1, 4, 10), activation.sigmoid, activation.sigmoid_prime)
+    EPOCH = 5000
 
-    for i in range(750):
-        network.backward_propagation(example_one, Vector([0]))
-        network.backward_propagation(example_two, Vector([1]))
-        network.backward_propagation(example_three, Vector([0]))
+    for item in [0.05, 0.1, 0.15, 0.2, 0.5, 1.0]:
 
-        predicted_output = []
-        predicted_output.append(network.forward_propagation(example_one))
-        predicted_output.append(network.forward_propagation(example_two))
-        predicted_output.append(network.forward_propagation(example_three))
+        cost = []
 
-        actual_output = [Vector(0), Vector(1), Vector(0)]
+        network = NeuralNetwork((4, 1, 5, 10), activation.sigmoid, activation.sigmoid_prime)
+        network.learn_rate = item
 
-        cost.append(network.cost_function(actual_output, predicted_output))
+        for i in range(EPOCH):
+            network.backward_propagation(example_one, Vector([0]))
+            network.backward_propagation(example_two, Vector([1]))
+            network.backward_propagation(example_three, Vector([0]))
 
-    print(network.forward_propagation(Vector([0,1,0,0])))
-    print(network.forward_propagation(Vector([1,0,0,0])))
+            predicted_output = []
+            predicted_output.append(network.forward_propagation(example_one))
+            predicted_output.append(network.forward_propagation(example_two))
+            predicted_output.append(network.forward_propagation(example_three))
 
-    plt.plot(cost)
+            actual_output = [Vector(0), Vector(1), Vector(0)]
+
+            cost.append(network.cost_function(actual_output, predicted_output))
+
+        print(network.forward_propagation(Vector([0,1,0,0])))
+        print(network.forward_propagation(Vector([1,0,0,0])))
+
+        plt.plot([i for i in range(EPOCH)], cost)
+
     plt.show()
