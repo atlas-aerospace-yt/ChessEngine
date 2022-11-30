325b8489c17bee5a907c1b8319bf6181b3fcae53
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-11-30 22:18:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """
-main file to test the libraries while under development
+Example use of this neural network library.
+This demo has 4 examples of a vector of length 4.
+Then the model trains over 750 iterations and shows the cost against epoch.
+The output for two unseen combinations are then shown.
 
 TODO: Overflow handling - more testing
 """
@@ -13,16 +16,25 @@ from vector import Vector, activation
 
 if __name__ == "__main__":
 
+    # Declaring examples
     example_inputs = [Vector([0,0,0,0]),
                     Vector([1,0,1,0]),
                     Vector([0,1,1,0])]
 
     example_outputs = [Vector(0), Vector(1), Vector(0)]
 
+    # Initialising network
     training_method = BackProp(activation.sigmoid_prime)
     model = NeuralNetwork((4, 1, 5, 5), activation.sigmoid, training_method)
 
-    cost = model.train_network(example_inputs, example_outputs, 10000)
+    # Training the network
+    cost = model.train_network(example_inputs, example_outputs, 750)
 
+    # Outputs
+    print(model)
+    print(f"Prediction for {Vector([1,1,1,1])} is {model.forward_propagation(Vector([1,1,1,1]))}")
+    print(f"Prediction for {Vector([0,1,1,1])} is {model.forward_propagation(Vector([0,1,1,1]))}")
+
+    # Show Graph
     plt.plot(cost)
     plt.show()
