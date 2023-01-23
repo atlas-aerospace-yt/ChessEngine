dcb0cf59304d7c7c1f92dc3edd49405b60f03ada
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-01-23 22:51:06 +0000

    Update test_image_recognition.py

--- test_image_recognition.py
+++ test_image_recognition.py
@@ -9,6 +9,5 @@ from model.image import ImageRecognition
 recogniser = ImageRecognition(f"{os.getcwd()}\\example_data")
 recogniser.learn_images(epoch=2000)
 
-print(recogniser.recognise(
-    f"{os.getcwd()}\\example_data\\perry\\perry1.png", show=True))
+print(recogniser.recognise(f"{os.getcwd()}\\example_data\\perry\\perry1.png", show=True))
 print(recogniser.recognise(f"{os.getcwd()}\\example_data\\not_perry\\notperry1.png", show=True))
